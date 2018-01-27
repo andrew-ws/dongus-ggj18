563d9a242eba8cf5e87908fc0d87f692f38d261b
ashanhol <orashaboogie@gmail.com>
2018-01-27 14:20:19 -0500

    add terminal controller and lane controller scripts

---
+++ Assets/Scripts/LaneController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneController : MonoBehaviour {
+
+    const float HackTime = 5.0f;
+
+    float hackTimeElapsed;
+
+    public bool isHacked; //terminal will not set hackedby if already hacked
+    bool isHacking;
+
+    public int HackedBy;
+
+    // Use this for initialization
+    void Start () {
+
+        isHacked = false;
+        isHacking = false;
+        hackTimeElapsed = 0;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (!isHacked)
+            //If opponent minion collides with terminal
+            if (isHacking)
+            {
+                Transmitting();
+            }
+
+    }
+
+    //process of hacking. call by terminal after minion collides
+    public void Transmitting()
+    {
+        //if this is called by terminal
+        if (!isHacking)
+            isHacking = true;
+
+        hackTimeElapsed += Time.deltaTime;
+
+        if (hackTimeElapsed >= HackTime)
+            isHacked = true;
+    }
+
+    //call this to stop hacking if minion dies 
+    public void SeverConnection()
+    {
+        isHacking = false;
+        hackTimeElapsed = 0;
+    }
+}
---
+++ Assets/Scripts/TerminalController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalController : MonoBehaviour {
+
+    public Transform MissileSpawn;
+
+    public int PlayerIndex; //SET ONLY TO 1 or 2 for player 1 and player 2 
+
+    public GameObject Lane;
+    public LaneController laneController;
+
+	// Use this for initialization
+	void Start () {
+        laneController = Lane.GetComponent<LaneController>();	
+	}
+	
+    //If a minion from the opposite team collides with the terminal, start hacking.
+    //And change "HackedBy" to the opponent 
+    private void OnTriggerEnter(Collider other)
+    {
+        //TODO: make sure PlayerIndex is in Minion
+        if(other.tag == "minion" && other.gameObject.GetComponent<Minion>().PlayerIndex != PlayerIndex)
+        {
+            if (!laneController.isHacked)
+            {
+                laneController.HackedBy = PlayerIndex;
+                laneController.Transmitting();
+            }
+        }
+    }
+
+    //If the minion from opposite team dies, sever connection.
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "minion" && other.gameObject.GetComponent<Minion>().PlayerIndex != PlayerIndex)
+        {
+            laneController.SeverConnection();
+        }
+    }
+}
