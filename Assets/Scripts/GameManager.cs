13a5f66eeda232790d67546fb18a72dea8638b73
Nikki Lombardo <hello@charblar.com>
2018-01-27 14:51:59 -0500

    Added game manager with audio source

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private AudioSource musicSource;
+
+	void Start ()
+    {
+        //we don't need to play the song here since we can use the
+        //'Play On Awake' property for the audio source. (nikki)
+        musicSource = GetComponent<AudioSource>();
+	}
+}
