aceda74bf2c39b9679f4b133b916ff793086a54e
Andrew Sherman <asherman50@gmail.com>
2018-01-27 20:34:49 -0500

    empty out the packet and payload
    not sure if we'll need these or not but let's keep them in anyway

--- Assets/Scripts/Minion/Packet.cs
+++ Assets/Scripts/Minion/Packet.cs
@@ -3,15 +3,5 @@ using UnityEngine;
 
 namespace GG18.Minions {
 	public class Packet : Minion {
-
-		// Use this for initialization
-		void Start () {
-			
-		}
-		
-		// Update is called once per frame
-		void Update () {
-			
-		}
 	}
 }
